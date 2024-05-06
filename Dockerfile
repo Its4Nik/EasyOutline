6fea44c0a0f8a93d8a7d771e0564f13dfb70a63a
ItsNik <106100177+Its4Nik@users.noreply.github.com>
2024-05-06 11:11:29 +0000

    commit

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM node:17.8-alpine3.15
+
+EXPOSE 8061
+
+WORKDIR /iframely
+
+# Create new non-root user
+RUN addgroup -S iframelygroup && adduser -S iframely -G iframelygroup
+RUN apk add g++ make python3
+
+# This will change the config to `config.<VALUE>.js` and the express server to change its behaviour.
+# You should overwrite this on the CLI with `-e NODE_ENV=production`.
+ENV NODE_ENV=local
+
+## Utilize docker layer cache
+COPY package.json yarn.lock /iframely/
+RUN yarn install --pure-lockfile --production
+
+COPY ./iframely /iframely
+
+RUN chown -R iframely /iframely/config.local.js
+
+USER iframely
+
+ENTRYPOINT [ "/iframely/docker/entrypoint.sh" ]
--- setup.sh
+++ setup.sh
@@ -4,6 +4,12 @@ FILE_LOCATION=""
 RANDKEY1="$(openssl rand -hex 32)"
 RANDKEY2="$(openssl rand -hex 32)"
 
+iframely_url="http://127.0.0.1:8061"
+iframely_api=""
+
+
+#----------------------------------------------------------------------------------------
+
 echo "# –––––––––––––––– REQUIRED ––––––––––––––––
 
 NODE_ENV=production" > docker.env
@@ -88,13 +94,23 @@ getSMTP(){
     echo "E-Mail: $email - Reply: $reply"
 
     confirm getSMTP
+
+    addToConf "# --------- EMAIL ----------"
+    addToConf SMTP_HOST=$host
+    addToConf SMTP_PORT=$port
+    addToConf SMTP_USERNAME=$user
+    addToConf SMTP_PASSWORD=$passwd
+    addToConf SMTP_FROM_EMAIL=$email
+    addToConf SMTP_REPLY_EMAIL=$reply
 }
 
 getIframeLy(){
-    iframely_url="http://127.0.0.1:8061"
-    iframely_api=""
     echo "Do you want to use the defaults?"
-    confirm customIframely
+    addToConf "# -------- iFramely --------"
+    confirm customIframely 
+    addToConf IFRAMELY_URL=$iframely_url
+    addToConf IFRAMELY_API_KEY=$iframely_api
+    
 }
 
 customIframely(){
@@ -103,9 +119,23 @@ customIframely(){
     confirm customIframely
 }
 
+addToConf(){
+    echo "" >> docker.env
+    echo "$@" >> docker.env
+}
+
 defaultParams
 getFileLocation
 randomKey
 getUrlAndPort
 getSMTP
 getIframeLy
+
+echo
+echo "Continuing..."
+sleep 3
+
+docker build -t iframely:latest .
+
+rm ./Dockerfile
+rm -rf ./iframely
